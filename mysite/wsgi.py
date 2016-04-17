3dd2c68efd4d80e8eada397fdf21aae4ff361f6a
Chiara Corrado <kiaruzza@alice.it>
2016-04-17 21:01:01 +0200

    Modifica di wsgi

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for mysite project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+path = '/home/kiaruzza/my-first-blog'  # usa il tuo username qui
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
